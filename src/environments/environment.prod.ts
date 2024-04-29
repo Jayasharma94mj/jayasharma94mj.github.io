7de7f451f1261cb8f1f19e81cd688fccbcbe2680
Sharma <jaya.a.sharma@accenture.com>
2024-04-29 18:58:03 +0530

    final demo change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     //apiKey: '${{ secrets.API_KEY }}',
     apiKey: "a33d2aa573b349fdba35fd36018b9677",
-    model:'gpt-4-32k',
-    //model: 'gpt-35-UT'
+    //model:'gpt-4-32k',
+    model: 'gpt-35-UT'
   };
